6366f5dc9ff87fd0d900061215f7c72621a8c7f1
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-05-07 20:41:50 +0600

    footer done

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ import router from './Routes/Routes.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+<div>
+<React.StrictMode>
  <RouterProvider router={router} /> 
   </React.StrictMode>,
+</div>
 )
